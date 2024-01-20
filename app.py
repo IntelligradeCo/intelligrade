1e8969ccafa28a5e3ab090c2a2d95c72cb56528c
IntelligradeCo <157077786+IntelligradeCo@users.noreply.github.com>
2024-01-20 00:56:32 +0000

    test

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
+data = []
+
 # Class Types to GPA
 normal_classes_gpa = {
     "A+": 4.0,
@@ -57,11 +59,25 @@ def class_type():
 
 def user_selections():
     courses_number = st.number_input("How many courses would you like to calculate GPA for?", 1)
+    return courses_number
+
+def make_table(courses_number):
+    for i in range(courses_number):
+        class_name = st.text_input(f"Class Name {i+1}")
+        letter_grade = st.selectbox(f"Letter Grade {i+1}", ('A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D', 'F'))
+        class_type = st.selectbox(f"Class Type {i+1}", ('Normal', 'Honors', 'AP', 'IB'))
+        data.append([class_name, letter_grade, class_type])
     
+        df = pd.DataFrame(data, columns=['Class Name', 'Letter Grade', 'Class Type'])
+    st.table(df)
+
+courses_number = user_selections()
+make_table(courses_number)
 
 def convert_gpa(selected_class):
     if selected_class == 0:
         print("HI")
 
 user_selections()
+make_table()
 
