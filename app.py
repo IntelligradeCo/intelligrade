7eaf9d04654b18ed9b817e220cebd7081a2fed07
IntelligradeCo <157077786+IntelligradeCo@users.noreply.github.com>
2024-01-20 01:40:20 +0000

    name:(

--- app.py
+++ app.py
@@ -50,20 +50,16 @@ ib_ap_classes_gpa = {
     "D": 2.0,
     "F": 0.0
 }
-def name():
-    name = st.text_input("What is your name?")
-    return name
 
 def user_selections():
     courses_number = st.number_input("How many courses would you like to calculate GPA for?", 1)
     return courses_number
 
-def make_table(num_rows, name):
+def make_table(num_rows):
     # Create an empty DataFrame with the desired number of rows
     df = pd.DataFrame(columns=['Class Name', 'Letter Grade', 'Class Type', 'GPA'],
-                      index=range(num_rows+1))
-    df.loc[1] = [name]
-
+                      index=range(num_rows))
+    
     # Display the table with input fields for each cell
     for i in range(num_rows):
         class_name = st.text_input(f"Class Name {i+1}", key=f"class_name_{i}")
@@ -85,5 +81,4 @@ def make_table(num_rows, name):
     st.table(df)
 
 courses_number = user_selections()
-name = name()
-make_table(courses_number,name)
+make_table(courses_number)
