b7b2ed7394bc42fc457682b22abe819d243a1c6e
IntelligradeCo <157077786+IntelligradeCo@users.noreply.github.com>
2024-01-28 12:22:13 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -54,39 +54,39 @@ ib_ap_classes_gpa = {
 
 
 # Function to calculate average GPA
-def gpa_calculator():
-    def calculate_avg_gpa(df):
-        total_gpa = df['GPA'].sum()
-        avg_gpa = total_gpa / len(df)
-        return avg_gpa
+
+def calculate_avg_gpa(df):
+    total_gpa = df['GPA'].sum()
+    avg_gpa = total_gpa / len(df)
+    return avg_gpa
+
+# Function to ask user inputs for number of courses
+def user_selections():
+    courses_number = st.number_input("How many courses would you like to calculate GPA for?", 1)
+    return courses_number
+
+# Function to make and empty table using Pandas DataFrame
+def make_table(num_rows):
+    df = pd.DataFrame(columns=['Class Name', 'Letter Grade', 'Class Type', 'GPA'],
+                      index=range(num_rows))
     
-    # Function to ask user inputs for number of courses
-    def user_selections():
-        courses_number = st.number_input("How many courses would you like to calculate GPA for?", 1)
-        return courses_number
-    
-    # Function to make and empty table using Pandas DataFrame
-    def make_table(num_rows):
-        df = pd.DataFrame(columns=['Class Name', 'Letter Grade', 'Class Type', 'GPA'],
-                          index=range(num_rows))
-        
-        # Display the table with input fields for each cell
-        for i in range(num_rows):
-            class_name = st.text_input(f"Class Name {i+1}", key=f"class_name_{i}")
-            letter_grade = st.selectbox(f"Letter Grade {i+1}", ('A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D', 'F'), key=f"letter_grade_{i}")
-            class_type = st.selectbox(f"Class Type {i+1}", ('Normal', 'Honors', 'AP', 'IB'), key=f"class_type_{i}")
-            if class_type == 'Normal':
-                gpa = normal_classes_gpa[letter_grade]
-            elif class_type == 'Honors':
-                gpa = honors_classes_gpa[letter_grade]
-            elif class_type == 'IB' or "AP":
-                gpa = ib_ap_classes_gpa[letter_grade]
-    
-            df.loc[i] = [class_name, letter_grade, class_type, gpa]
-            st.divider()
-    
-        # Displaying updated table
-        st.table(df)
-        # Calling Average GPA function
-        avg_gpa = calculate_avg_gpa(df)
-        st.write(f"Average GPA: {avg_gpa:.2f}")
+    # Display the table with input fields for each cell
+    for i in range(num_rows):
+        class_name = st.text_input(f"Class Name {i+1}", key=f"class_name_{i}")
+        letter_grade = st.selectbox(f"Letter Grade {i+1}", ('A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D', 'F'), key=f"letter_grade_{i}")
+        class_type = st.selectbox(f"Class Type {i+1}", ('Normal', 'Honors', 'AP', 'IB'), key=f"class_type_{i}")
+        if class_type == 'Normal':
+            gpa = normal_classes_gpa[letter_grade]
+        elif class_type == 'Honors':
+            gpa = honors_classes_gpa[letter_grade]
+        elif class_type == 'IB' or "AP":
+            gpa = ib_ap_classes_gpa[letter_grade]
+
+        df.loc[i] = [class_name, letter_grade, class_type, gpa]
+        st.divider()
+
+    # Displaying updated table
+    st.table(df)
+    # Calling Average GPA function
+    avg_gpa = calculate_avg_gpa(df)
+    st.write(f"Average GPA: {avg_gpa:.2f}")
