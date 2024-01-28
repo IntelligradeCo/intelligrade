d4c9d4c1d68178484c92ee6c3ebf08c8e6494ab0
IntelligradeCo <157077786+IntelligradeCo@users.noreply.github.com>
2024-01-28 21:00:28 +0000

    main

--- app.py
+++ app.py
@@ -7,11 +7,8 @@ from utils import *
 st.title("GPA Calculator")
 
 
-# Main content area
-if selected_feature == "GPA Calculator":
-    # Calling make_table function
-    courses_number = user_selections()
-    make_table(courses_number)
+courses_number = user_selections()
+make_table(courses_number)
 
 
 
