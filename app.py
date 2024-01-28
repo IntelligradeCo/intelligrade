8112a99ace8d50400afeb752cc2a38da49b3c2a1
IntelligradeCo <157077786+IntelligradeCo@users.noreply.github.com>
2024-01-28 12:18:48 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from utils import *
 
+# App title and layout
+st.title("GPA Calculator")
+
+# Sidebar navigation
+st.sidebar.title("Navigation")
+selected_feature = st.sidebar.selectbox("Select a Feature:", ["GPA Calculator", "Grade Tracker"])
+
+# Main content area
+if selected_feature == "GPA Calculator":
+    gpa_calculator
+elif selected_feature == "Grade Tracker":
+    grade_tracker
+
+
 # Calling make_table function
 courses_number = user_selections()
 make_table(courses_number)
