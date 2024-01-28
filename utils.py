d5d67341d7219a6df18b193964676c4027113012
IntelligradeCo <157077786+IntelligradeCo@users.noreply.github.com>
2024-01-28 21:42:35 +0000

    download

--- utils.py
+++ utils.py
@@ -68,6 +68,10 @@ def user_selections():
     courses_number = st.number_input("How many courses would you like to calculate GPA for?", 1)
     return courses_number
 
+# Convert dataframe to csv
+def convert_df(df):
+    return df.to_csv.encode('utf-8')
+
 # Function to make and empty table using Pandas DataFrame
 def make_table(num_rows):
     name = user()
@@ -97,8 +101,14 @@ def make_table(num_rows):
     # Calling Average GPA function
     avg_gpa = calculate_avg_gpa(df)
     st.write(f"Average GPA: {avg_gpa:.2f}")
-    df.to_csv(f"Unofficial transcript for {name}")
-    st.download_button("Unofficial Transcript", "gpa_data.csv")
+    unofficial_transcript = convert_df(df)
+    
+    st.download_button(
+    label="Download data as CSV",
+    data=unofficial_transcript,
+    file_name='large_df.csv',
+    mime='text/csv',
+)
 
 
     
