53b2672047922c3f6ca0efd52d9d6f168463b46b
IntelligradeCo <157077786+IntelligradeCo@users.noreply.github.com>
2024-01-28 21:39:29 +0000

    a

--- utils.py
+++ utils.py
@@ -97,6 +97,8 @@ def make_table(num_rows):
     # Calling Average GPA function
     avg_gpa = calculate_avg_gpa(df)
     st.write(f"Average GPA: {avg_gpa:.2f}")
+    df.to_csv(f"Unofficial transcript for {name}")
+    st.download_button("Unofficial Transcript", "gpa_data.csv")
 
 
     
